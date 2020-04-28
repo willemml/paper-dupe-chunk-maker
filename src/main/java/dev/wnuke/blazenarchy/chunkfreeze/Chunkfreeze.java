593f343b2da8bc33407f4c4537d57a30e3a85ce9
willem <willem@leit.so>
2020-04-28 14:45:45 +0200

    Basic plugin init

---
+++ src/main/java/dev/wnuke/blazenarchy/chunkfreeze/Chunkfreeze.java
@@ -0,0 +1,22 @@
+package dev.wnuke.blazenarchy.chunkfreeze;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Blazenarchy's ChunkFreeze. Stops a chunk from saving.
+ *
+ * @author wnuke
+ */
+public final class Chunkfreeze extends JavaPlugin {
+    public static final String VERSION = "1.0.0";
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Loaded ChunkFreeze version " + VERSION + " by wnuke.");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Disabling ChunkFreeze...");
+    }
+}
